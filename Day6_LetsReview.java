2b16c37586aaecfb6e6c2a2f5e60c66acb581e60
Manish Kumar <manishkumarmdb@users.noreply.github.com>
2016-01-16 23:22:26 +0530

    Create Day6_LetsReview.java

---
+++ Day6_LetsReview.java
@@ -0,0 +1,28 @@
+package com.hackerrank.contests.daysOfCode_30;
+
+import java.util.Scanner;
+
+public class Day6_LetsReview {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		@SuppressWarnings("resource")
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			int count = 0;
+			while (count != (n - (i + 1))) {
+				System.out.print(" ");
+				count++;
+			}
+			for (int j = (n - i); j <= n; j++) {
+				System.out.print("#");
+			}
+			System.out.println();
+			
+		}
+	}
+
+}
