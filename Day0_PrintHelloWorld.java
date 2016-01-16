e694adc61f1bd026b7fd8175c65b95f023832250
Manish Kumar <manishkumarmdb@users.noreply.github.com>
2016-01-16 23:16:46 +0530

    Create Day0_PrintHelloWorld.java

---
+++ Day0_PrintHelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+       public static void main(String[] args) {
+        
+          // Write your print statements here!! 
+          // Template: System.out.println("what you want to print");
+           String string1 = "Hello World.";
+           String string2 = "Welcome to 30 Days of Code.";
+           
+           System.out.println(string1);
+           System.out.println(string2);
+        
+       }
+   }
