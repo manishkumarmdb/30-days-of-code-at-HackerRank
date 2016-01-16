694a9de4594be57e6a35cba09a15246650db3d8c
Manish Kumar <manishkumarmdb@users.noreply.github.com>
2016-01-16 23:18:51 +0530

    Create Day1_DataTypes.java

---
+++ Day1_DataTypes.java
@@ -0,0 +1,35 @@
+package com.hackerrank.contests.daysOfCode_30;
+
+import java.util.Arrays;
+
+@SuppressWarnings("unused")
+public class Day1_DataTypes {
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+
+		double value1 = 5.35;
+		System.out.println("Primitive : " + Double.TYPE);
+		char value2 = 'a';
+		System.out.println("Primitive : " + Character.TYPE);
+		boolean value3 = false;
+		System.out.println("Primitive : " + Boolean.TYPE);
+		int value4 = 100;
+		System.out.println("Primitive : " + Integer.TYPE);
+		String value5 = "I am a code monkey";
+		System.out.println("Referenced : " + String.class.getSimpleName());
+		boolean value6 = true;
+		System.out.println("Primitive : " + Boolean.TYPE);
+		double value7 = 17.3;
+		System.out.println("Primitive : " + Double.TYPE);
+		char value8 = 'c';
+		System.out.println("Primitive : " + Character.TYPE);
+		String value9 = "derp";
+		System.out.println("Referenced : " + String.class.getSimpleName());
+		//Arrays[] value6 = {};
+		//System.out.println("Reference : " + value6.getClass().getSimpleName());
+		
+	}
+
+}
