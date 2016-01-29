74a547d8086286424ef64ac2fff6d931edca3b73
Manish Kumar <manishkumarmdb@users.noreply.github.com>
2016-01-29 23:12:56 +0530

    Create Day27_TestingPart_II.java

---
+++ Day27_TestingPart_II.java
@@ -0,0 +1,27 @@
+package com.hackerrank.contests.daysOfCode_30;
+
+
+/**
+ * @author manishkumarmdb
+ *
+ */
+public class Day27_TestingPart_II {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(5);
+		System.out.println(5 + " " + 3);
+		System.out.println(2 + " " + 3 + " " + 0 + " " + 1 + " " + -2);
+		System.out.println(4 + " " + 2);
+		System.out.println(-1 + " " + 0 + " " + -2 + " " + 6);
+		System.out.println(7 + " " + 4);
+		System.out.println(1 + " " + 2 + " " + 3 + " " + 4 + " " + 0 + " " + -1	+ " " + -5);
+		System.out.println(3 + " " + 2);
+		System.out.println(1 + " " + -2 + " " + 0);
+		System.out.println(6 + " " + 4);
+		System.out.println(4 + " " + 2 + " " + 5 + " " + 3 + " " + -1 + " " + 0);
+
+	}
+
+}
